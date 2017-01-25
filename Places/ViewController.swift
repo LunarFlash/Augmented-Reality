294e9349e67580fc60a007ad360ac96f9120730b
Wang, Terry <terry.wang@accenture.com>
2017-01-24 20:19:07 -0500

    fixed google web api console settings

--- Places/ViewController.swift
+++ Places/ViewController.swift
@@ -8,6 +8,8 @@ import CoreLocation
 class ViewController: UIViewController {
   
   fileprivate let locationManager = CLLocationManager()
+  fileprivate var startedLoadingPOIs = false
+  fileprivate var places = [Place]()
   
   @IBOutlet weak var mapView: MKMapView!
   override func viewDidLoad() {
@@ -29,3 +31,36 @@ class ViewController: UIViewController {
   
 }
 
+extension ViewController: CLLocationManagerDelegate {
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    if locations.count > 0 {
+      let location = locations.last!
+      print("Accuracy: \(location.horizontalAccuracy)")  // radius around the current location. If you have a value of 50, it means that the real location can be in a circle with a radius of 50 meters around the position stored in location.
+      if location.horizontalAccuracy < 100 {
+        manager.stopUpdatingLocation()  // save battery, we don't need more than 1 location value with accuracy of 100 meters
+        let span = MKCoordinateSpan(latitudeDelta: 0.014, longitudeDelta: 0.014)
+        let region = MKCoordinateRegion(center: location.coordinate, span: span)
+        mapView.region = region
+        
+        // --
+        if !startedLoadingPOIs {
+          startedLoadingPOIs = true
+          
+          let loader = PlacesLoader()
+          loader.loadPOIS(location: location, radius: 1000, handler: { (placesDict, error) in
+            
+            if let dict = placesDict {
+              print(dict)
+              
+            }
+            
+          })
+        }
+        
+        
+      }
+    }
+  }
+  
+}
